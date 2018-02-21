9f54e476258b5b984c7422444d9e434b62eb6a47
graceo1990 <gortega1@hawk.iit.edu>
2018-02-20 21:13:47 -0600

    Add files via upload
    added javascript

---
+++ site.js
@@ -0,0 +1,28 @@
+//jQuery 3.x-style ready event and locally scoped $
+/* Email Validation */ 
+jQuery(function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+  {
+    return (true)
+  }
+    alert("Invalid email address entered!")
+    return (false)
+}};
+
+/* Form Validation */
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Fill out Name Please!");
+        return false;
+    }
+};
+
+
+
+
+
+
+
